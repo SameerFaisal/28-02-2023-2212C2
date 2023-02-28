e1ffc47a8dc2ee68f79eaad4a4932a59365eb2cf
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-02-28 15:00:11 +0500

    28-02-2023

---
+++ 28-02-2023/app.js
@@ -0,0 +1,72 @@
+// let country=prompt("Enter a country name")
+
+// if(country==="pakistan"){
+// document.write("Welcome to Pakistan")
+// }
+// else if(country==="india"){
+// document.write("Welcome to india")
+// }
+// else if(country==="nepal"){
+// document.write("Welcome to nepal")
+// }
+// else{
+//     document.write("Country not found")
+// }
+// document.write("<h1>Area Calculator</h1><br>")
+// let choice=+prompt("Enter 1 for Rectangle. Enter 2 for square. Enter 3 to exit")
+
+// if(choice===1){
+//     let length=prompt("Enter length of rectangle")
+//     let width=prompt("Enter width of rectangle")
+//     let rectArea=(parseFloat(length)*parseFloat(width))
+//     document.write("The Area of Rectangle is: "+rectArea)
+// }
+// else if(choice===2){
+//     let length=+prompt("Enter length of Square")
+//     let sqArea=length**2
+//     document.write("The Area of square is: "+sqArea)
+// }
+// else if(choice===3){
+//     document.write("Thankyou for using my calculator")
+// }
+// else{
+//     document.write("Provided input was not valid")
+// }
+
+// let num1=prompt("Enter first number")
+// let num2=prompt("Enter second number")
+// let result=0
+// let choice=prompt("Enter + to add. Enter - to subtract. Enter * to multiply. Enter / to divide ")
+
+// switch(choice){
+//     case "+":
+//         result=num1+num2
+//         document.write("Addition Result is: "+result)
+//         break
+//     case "-":
+//         result=num1-num2
+//         document.write("Subtraction Result is: "+result)
+//         break
+//     case "*":
+//         result=num1*num2
+//         document.write("Multiplication Result is: "+result)
+//         break
+//     case "/":
+//         result=num1/num2
+//         document.write("Division Result is: "+result)
+//         break
+//     default:
+//         document.write("Entered operator not found")
+    
+// }
+
+
+// let num1=prompt("Enter first number")
+// let num2=prompt("Enter second number")
+// result=num1+(-num2)
+// document.write("Addition Result is: "+result)
+
+
+
+
+
